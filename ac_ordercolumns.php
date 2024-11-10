3246ab95f829422095ac81d0af8cc99a202575e8
Guillaume Chinal <guiiix@invisiblethingslab.com>
2024-11-10 13:47:18 +0100

    update copyright

--- ac_ordercolumns.php
+++ ac_ordercolumns.php
@@ -1,6 +1,6 @@
 <?php
 /**
-* 2007-2016 PrestaShop
+* Copyright 2024 - Foxchip
 *
 * NOTICE OF LICENSE
 *
@@ -18,8 +18,8 @@
 * versions in the future. If you wish to customize PrestaShop for your
 * needs please refer to http://www.prestashop.com for more information.
 *
-*  @author    PrestaShop SA <contact@prestashop.com>
-*  @copyright 2007-2016 PrestaShop SA
+*  @author    Foxchip <contact@foxchip.com>
+*  @copyright 2024 Foxchip
 *  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
 *  International Registred Trademark & Property of PrestaShop SA
 */
